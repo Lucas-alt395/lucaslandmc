1411de7b9cb347a595fc6a70f77c0d0d831ec015
Lucas-alt395 <lucasbednar8@gmail.com>
2026-05-23 13:20:38 +0200

    2

--- js/navbar.js
+++ js/navbar.js
@@ -8,8 +8,8 @@ class LLNavbar extends HTMLElement {
         this.innerHTML = `
         <nav class="topbar">
             <div class="logo-area">
-                <a href="/" style="text-decoration: none;"><h1>BAPI</h1></a>
-                <p class="tagline">the game linking platform</p>
+                <a href="/" style="text-decoration: none;"><h1>Lucasland</h1></a>
+                <p class="tagline">the best country</p>
             </div>
             <div class="nav-links">
                 <a href="/login/login.html" class="light-text">Login</a>
@@ -28,9 +28,8 @@ class LLFooter extends HTMLElement {
                 <p class="footer-logo">Lucasland</p>
                 <p>De rest is in het Engels.</p>
                 <p class="disclaimer">
-                    BAPI is an independent game linking platform. 
-                    <strong>Roblox</strong> and <strong>Blox</strong> are trademarks of Roblox Corporation. 
-                    This site is not affiliated with, authorized, or endorsed by Roblox Corp.
+                    <strong>Minecraft</strong> is a trademark of Mojang Studios (Microsoft Corporation). 
+                    This site is not affiliated with, authorized, or endorsed by Mojang Studios.
                 </p>
                 <p class="disclaimer">
                     For the icons: thanks and credit to <a href="https://icon-icons.com/authors/20-iconshock" title="Iconshock link" class="light-text">Iconshock</a> and <a href="https://icon-icons.com" title="Iconshock link" class="light-text">icon-icons.com</a> for the icons used on this site. They are not affiliated with BAPI. I accept their license. (<a href="https://creativecommons.org/licenses/by/4.0/" title="CC link" class="light-text">CC-BY-4.0</a>)
